a5220efe99ec2bcee9d9561ea727626d80b64a0f
Danil Tkachenko <danil.tkachenko@getmore.cz>
2023-05-21 01:00:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace SocketWorkSecond
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			// Создаем клиентский сокет
+			// Создаем серверный сокет
+			Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			// Привязываем его к определенному IP-адресу и порту
+			IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 12345);
+			serverSocket.Bind(endPoint);
+
+			// Начинаем прослушивание
+			serverSocket.Listen(10);
+			// Принимаем клиента
+
+			// Получаем сообщение
+			Socket clientSocket = serverSocket.Accept();
+			int received;
+			while (true)
+			{
+
+				byte[] buffer = new byte[1024];
+
+
+				try
+				{
+					received = clientSocket.Receive(buffer);
+				}
+				catch (Exception)
+				{
+					received = 0;
+				}
+
+
+
+				if (received == 0)
+				{
+
+					clientSocket.Shutdown(SocketShutdown.Both);
+
+					clientSocket.Close();
+
+					clientSocket = serverSocket.Accept();
+
+				}
+				// Преобразуем байты обратно в строку
+				string receivedMessage = Encoding.UTF8.GetString(buffer, 0, received);
+
+				// Выводим сообщение
+				if (receivedMessage != string.Empty)
+				{
+					Console.WriteLine("Received: " + receivedMessage);
+				}
+
+
+
+
+			}
+
+			// Закрываем сокеты
+			clientSocket.Shutdown(SocketShutdown.Both);
+			clientSocket.Close();
+
+
+
+
+
+
+
+		}
+	}
+}
