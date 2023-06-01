2f18f23a50c7700aed3d088c08d7fd70829f2dd5
Danil Tkachenko <danil.tkachenko@getmore.cz>
2023-06-01 20:08:20 +0200

    last change

--- Program.cs
+++ Program.cs
@@ -21,60 +21,56 @@ namespace SocketWorkSecond
 			serverSocket.Listen(10);
 			// Принимаем клиента
 
-			// Получаем сообщение
-			Socket clientSocket = serverSocket.Accept();
-			int received;
 			while (true)
 			{
-
-				byte[] buffer = new byte[1024];
-
-
-				try
-				{
-					received = clientSocket.Receive(buffer);
-				}
-				catch (Exception)
-				{
-					received = 0;
-				}
-
-
-
-				if (received == 0)
-				{
-
-					clientSocket.Shutdown(SocketShutdown.Both);
-
-					clientSocket.Close();
-
-					clientSocket = serverSocket.Accept();
-
-				}
-				// Преобразуем байты обратно в строку
-				string receivedMessage = Encoding.UTF8.GetString(buffer, 0, received);
-
-				// Выводим сообщение
-				if (receivedMessage != string.Empty)
-				{
-					Console.WriteLine("Received: " + receivedMessage);
-				}
-
-
-
-
+				Socket clientSocket = serverSocket.Accept();
+				Console.WriteLine("User Join");
+				Thread clientThread = new Thread(() => HandleClient(clientSocket));
+				clientThread.Start();
 			}
 
-			// Закрываем сокеты
-			clientSocket.Shutdown(SocketShutdown.Both);
-			clientSocket.Close();
+			void HandleClient(Socket clientSocket)
+			{
+				byte[] buffer = new byte[1024];
+				int received;
+				while (true)
+				{
+					try
+					{
+						received = clientSocket.Receive(buffer);
+					}
+					catch (Exception)
+					{
+						Console.WriteLine("Client disconnected forcefully.");
+						break;
+					}
 
+					if (received == 0)
+					{
+						Console.WriteLine("Client disconnected gracefully.");
+						break;
+					}
 
+					string receivedMessage = Encoding.UTF8.GetString(buffer, 0, received);
+					if (receivedMessage != string.Empty)
+					{
+						Console.WriteLine("Received: " + receivedMessage);
+					}
+				}
+
+				clientSocket.Shutdown(SocketShutdown.Both);
+				clientSocket.Close();
+			}
 
 
 
 
 
 		}
+
+		
+
+
+
 	}
 }
